8e9a10374ce75008ef467e1d3d7fa3780e6b8108
lailaregBom <lailaregina46@gmail.com>
2022-11-10 13:30:07 -0300

    inclusao do projeto

---
+++ api.automacao.keenggo/src/test/java/validar/name/Professional.java
@@ -0,0 +1,28 @@
+package validar.name;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+public class Professional {
+	
+	Response response;
+	
+	@Test
+	public void validarCampoName() {
+		response = RestAssured.get("https://api.trello.com/1/actions/592f11060f95a3d3d46a987a/list");
+		
+		String name = response.getBody().path("name", "name do empregado");
+		
+		System.out.println(name);
+		assertTrue(name.contains("Professional"));
+		
+	}
+		
+	
+
+	
+}
